118fa93fbd7a15dfd7d85c92c41ab33963323980
Serey <sereymorm@gmail.com>
2017-06-09 12:44:31 +0000

    Initialized Container

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername = "serey-db.cp3iwujbajvo.us-east-1.rds.amazonaws.com";
+$dbname = "choices";
+$username = "sereydb";
+$password = "password781";
+
+try {
+    $pdo = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
+    echo "Successfully connected to the Database!\n";
+} catch (PDOException $err) {
+    die($err->getMessage());
+}
