c4ec2b7b78133f9e50526e531961a876cd8c09e5
Serey <sereymorm@gmail.com>
2017-06-13 12:51:06 +0000

    more tests

--- src/index.php
+++ src/index.php
@@ -12,4 +12,4 @@ try {
     die($err->getMessage());
 }
 
-echo "Pipeline Container 1: Updated Auto build and deploy: test 4\n\n";
+echo "Pipeline Container 1: Updated Auto build and deploy: test five \n\n";
